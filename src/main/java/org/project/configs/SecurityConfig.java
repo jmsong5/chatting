5e64a03d3a8924f196bc18d9a039e2360defef45
jmsong5 <jmsong@taihoinst.com>
2023-09-25 17:01:21 +0900

    사이트 기본 설정 진행중

--- src/main/java/org/project/configs/SecurityConfig.java
+++ src/main/java/org/project/configs/SecurityConfig.java
@@ -1,7 +1,18 @@
 package org.project.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws  Exception {
+        return  http.build();
+    }
+
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
